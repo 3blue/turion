957461ae8e1714792fbd16b48043cea555333c25
Evan McKinnon <evan.mckinnon.1@gmail.com>
2025-03-27 15:47:51 -0400

    Python API: Enable CORS

--- telemetry_api/main.py
+++ telemetry_api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 
 app = FastAPI(
@@ -6,4 +7,17 @@ app = FastAPI(
     docs_url=None,
     redoc_url=None
 )
+
+origins = [
+    "http://localhost:5173",  # frontend dev server
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # allow your frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
